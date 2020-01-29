e10de5d3028773a7ad7cb4177d204d7eb194572f
papa-seiji <iwillbestandbyme@gmail.com>
2020-01-29 12:30:05 +0900

    非同期通信

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-  validates :name, presence: true, uniqueness: true
   has_many :messages
+  validates :name, presence: true, uniqueness: true
 
   def show_last_message
     if (last_message = messages.last).present?
