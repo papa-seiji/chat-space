2e812691c74377d36729e8ae6a91f6e848937461
papa-seiji <iwillbestandbyme@gmail.com>
2020-02-03 21:26:32 +0900

    完結

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+      respond_to do |format|
+        format.html
+        format.json
+    end
+  end
+
   def edit
   end
 
@@ -17,11 +26,3 @@ class UsersController < ApplicationController
   end
 end
 
-def index
-  return nil if params[:keyword] == ""
-  @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
-  respond_to do |format|
-    format.html
-    format.json
-  end
-end
