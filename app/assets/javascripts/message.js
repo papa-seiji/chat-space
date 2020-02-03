dffadb0302b88d038d07cdaf436fa104f1296b9a
papa-seiji <iwillbestandbyme@gmail.com>
2020-02-03 15:20:28 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -58,6 +58,7 @@ $(function(){
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
+    var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
       url: url,
@@ -72,6 +73,7 @@ $(function(){
       $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
       $('form')[0].reset();
+      $('.form__submit').prop('disabled', false);
     })
       .fail(function(){
         alert('error');
